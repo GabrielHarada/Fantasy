9c4be33d808b430cb7ce0f7646098492a829dae0
GabrielHarada <gabhrda@gmail.com>
2023-05-23 10:11:39 -0300

    video4

--- Personagem.cpp
+++ Personagem.cpp
@@ -3,7 +3,7 @@
 Jogo::Personagem::Personagem::Personagem(const sf::Vector2f pos, const sf::Vector2f tam):
 	corpo(sf::RectangleShape(tam))
 {
-
+	corpo.setPosition(pos);
 }
 
 Jogo::Personagem::Personagem::~Personagem()
--- Principal.cpp
+++ Principal.cpp
@@ -2,18 +2,21 @@
 #include "Principal.h"
 
 Jogo::Principal::Principal() :
-	window(sf::VideoMode(800.0f, 600.0f), "Jogo++"), personagens()
+	window(sf::VideoMode(1280.0f, 1024.0f), "Jogo++"), personagens()
 {
-	Personagem::Jogador::Jogador* jogador = new Personagem::Jogador::Jogador(sf::Vector2f(100.0f, 200.0f), sf::Vector2f(50.0f, 50.0f));
+	Personagem::Jogador::Jogador* jogador = new Personagem::Jogador::Jogador(sf::Vector2f(0.0f, 0.0f), sf::Vector2f(50.0f, 50.0f));
 	Personagem::Inimigo::Inimigo* inimigo = new Personagem::Inimigo::Inimigo(sf::Vector2f(100.0f, 100.0f), sf::Vector2f(50.0f, 50.0f), jogador);
 
 	Personagem::Personagem* p1 = static_cast<Personagem::Personagem*>(jogador);
 	Personagem::Personagem* p2 = static_cast<Personagem::Personagem*>(inimigo);
 
-	personagens.push_back(p1);
 	personagens.push_back(p2);
+	personagens.push_back(p1);
 
 	executar();
+
+	delete jogador;
+	delete inimigo;
 }
 
 Jogo::Principal::~Principal() {
@@ -37,6 +40,8 @@ void Jogo::Principal::executar() {
 		window.clear();
 		for (int i = 0; i < personagens.size(); i++) {
 			personagens.at(i)->move();
+		}
+		for (int i = 0; i < personagens.size(); i++) {
 			window.draw(personagens.at(i)->getCorpo());
 		}
 		window.display();
